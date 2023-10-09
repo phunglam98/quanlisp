5b398a6d675278e116ef0fee88cccadb1986a033
phunglam98 <duclam9812@gmail.com>
2023-10-09 17:39:07 +0700

    l

---
+++ src/Manager/IManager.java
@@ -0,0 +1,10 @@
+package Manager;
+
+import java.util.ArrayList;
+
+public interface IManager<T> {
+    void add(T e);
+    void delete(int id);
+    void edit(int id,T t);
+    ArrayList<T> showAll();
+}
